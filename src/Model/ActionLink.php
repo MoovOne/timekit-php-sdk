ca2ec111d7ef17f00c3170a73c90043c3d497715
Tobias <tobias@roomtailors.de>
2019-07-03 17:05:44 +0200

    Added action link creation

--- src/Model/ActionLink.php
+++ src/Model/ActionLink.php
@@ -17,8 +17,13 @@ class ActionLink
             "booking_id" => $booking_id,
             "action" => $action
         ));
+
+        // Encode Header to Base64Url String
         $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+
+        // Encode Payload to Base64Url String
         $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+        // Create Signature Hash
         $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $apiKey, true);
         $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
         $jwt = $base64UrlHeader . '.' . $base64UrlPayload . '.' . $base64UrlSignature;
